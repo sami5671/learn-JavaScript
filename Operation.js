4feb693b2c6386c294d246c609c86aa538118804
sami5671 <samialam5671@gmail.com>
2023-07-29 00:46:07 +0600

    updated

--- Operation.js
+++ Operation.js
@@ -1,8 +1,24 @@
 var onionPrice = 20;
 var eggPrice = 10;
 
+// addition, substractoin
 var totalPrice = onionPrice + eggPrice;
-console.log(totalPrice);
+var priceDifference = onionPrice - eggPrice;
+
+// console.log(priceDifference);
+// console.log(totalPrice);
 // console.log(onionPrice);
 // console.log(eggPrice);
 // console.log(onionPrice + eggPrice);
+
+// multiplication
+var orangePrice = 20;
+var quantity = 7;
+var totalCost = orangePrice * quantity;
+// console.log(totalCost);
+
+// division
+var money = 500;
+var player = 10;
+var eachPlayer = money / player;
+console.log(eachPlayer);
