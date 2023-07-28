00541c695a0cd9a6162a190a5fc0af4f1d106e8b
sami5671 <samialam5671@gmail.com>
2023-07-29 00:27:29 +0600

    update

--- Variables_naming_convention.js
+++ Variables_naming_convention.js
@@ -1,21 +1,3 @@
-console.log(7);
-
-/*variable is a thing which will change in terms of the condition*/
-// Numaric
-var price = 15;
-var age = 12;
-var temperature = 38;
-
-// string
-var person = "abdul motin";
-var location = "savar dhaka";
-var special = "alia bhatt";
-
-//Boolean
-var serious = true;
-var isFullMark = true;
-var isSingle = false;
-
 // 1. variable name cannot be any keyword
 var false = 89;
 var return = true;
