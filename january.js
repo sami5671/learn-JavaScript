ef3822783817891a55643de20c380bb6c5379954
sami5671 <samialam5671@gmail.com>
2023-07-29 00:12:14 +0600

    first commit

---
+++ january.js
@@ -0,0 +1,17 @@
+console.log(7);
+
+/*variable is a thing which will change in terms of the condition*/
+// Numaric
+var price = 15;
+var age = 12;
+var temperature = 38;
+
+// string
+var person = "abdul motin";
+var location = "savar dhaka";
+var special = "alia bhatt";
+
+//Boolean
+var serious = true;
+var isFullMark = true;
+var isSingle = false;
