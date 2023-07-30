9e340d660b9cf711dbc939cbfd218bca934cc064
sami5671 <samialam5671@gmail.com>
2023-07-30 16:34:13 +0600

    update

---
+++ Variables.js
@@ -0,0 +1,29 @@
+// number type variables
+var sunglasPrice = 120;
+
+// string type variable
+var specialOneName = "sami alam";
+
+// boolean type variable
+var isSingle = true;
+
+// two types of number: Integer: 120, float, decimal: 2.42
+
+// variable naming conventation: rules, best practices
+/**
+ * 1. single word
+ * 2. no gap space
+ * 3. no quote
+ * 4. no keyword
+ * 5. cannot start with a number
+ * 6. should use cameCase
+ * 7. cannot contain dash
+ */
+// Math operation: +, -, *, /
+// Shorthand: +=, -=, ++, --
+
+// convert: parseInt, parseFloat
+
+// toFixed
+
+// remainder: vagses: %
