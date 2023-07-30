5795b297ac912e3378e162d089aa50654c7f0b8b
sami5671 <samialam5671@gmail.com>
2023-07-31 01:25:36 +0600

    update summary

---
+++ summar.js
@@ -0,0 +1,11 @@
+var destinations = ["sajek", "coxsbaxar", "nepal", "bali", "sp"];
+console.log(destinations[2]);
+
+destinations[2] = "thailand";
+console.log(destinations);
+
+var index = destinations.indexOf("bali");
+console.log(index);
+
+destinations.push("bhutan");
+destinations.pop();
