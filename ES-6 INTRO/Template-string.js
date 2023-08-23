b9376c36e48b71b239af469f997c94a363e466cc
sami5671 <samialam5671@gmail.com>
2023-08-23 11:38:24 +0600

    update

---
+++ ES-6 INTRO/Template-string.js
@@ -0,0 +1,26 @@
+const first = "sami";
+const last = "alam";
+const greet = "alo potash";
+
+const name = first + " " + last + " " + greet;
+console.log(name);
+
+const a = 10;
+const b = 20;
+const result = "The sum of " + a + " and " + b + " is " + (a + b);
+console.log(result);
+
+// using template string ``
+const math = `The sum of ${a} and ${b} is ${a + b}`;
+console.log(math);
+
+// -------
+const eamil = "Hi Jhon \n" + "cena cena lage \n" + "tumi mara mari korba naki";
+console.log(eamil);
+
+// using template string
+const challenge = `Jhon cena
+cena cena lage tmumi boro ocena
+mair dimu naki`;
+
+console.log(challenge);
